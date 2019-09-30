1399c9aa8edb54a6cf24724f2d50abbc7afc14ab
BELLAAJ Aiman <Aiman.BELLAAJ_EXT@bpce.fr>
2019-09-30 18:42:37 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+## Spring Zuul
+
+This module contains articles about Spring with Netflix Zuul
+
+### Relevant Articles: 
+- [Spring REST with a Zuul Proxy](http://www.baeldung.com/spring-rest-with-zuul-proxy)
---
+++ spring-zuul-ui/src/main/java/org/baeldung/config/UiWebConfig.java
@@ -0,0 +1,35 @@
+package org.baeldung.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class UiWebConfig implements WebMvcConfigurer {
+
+	@Bean
+	public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+		return new PropertySourcesPlaceholderConfigurer();
+	}
+
+	public void configureDefaultServletHandling(final DefaultServletHandlerConfigurer configurer) {
+		configurer.enable();
+	}
+
+	public void addViewControllers(final ViewControllerRegistry registry) {
+		registry.addViewController("/").setViewName("forward:/index");
+		registry.addViewController("/index");
+		registry.addViewController("/login");
+	}
+
+	public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+	}
+
+}
