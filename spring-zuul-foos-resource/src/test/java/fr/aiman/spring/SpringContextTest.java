11e99ec98ddf60297813ed90fded8cc355d24157
BELLAAJ Aiman <Aiman.BELLAAJ_EXT@bpce.fr>
2019-10-01 10:40:26 +0200

    second commit

---
+++ spring-zuul-foos-resource/src/test/java/fr/aiman/spring/SpringContextTest.java
@@ -0,0 +1,17 @@
+package fr.aiman.spring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import fr.aiman.spring.config.ResourceServerApplication;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = ResourceServerApplication.class)
+public class SpringContextTest {
+
+    @Test
+    public void whenSpringContextIsBootstrapped_thenNoExceptions() {
+    }
+}
